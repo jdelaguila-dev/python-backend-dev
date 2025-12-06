daf8556266d26e397d628d8943a4f475958dc92e
JorgeAntonio <jorge.antonio.delaguila@gmail.com>
2025-12-05 20:09:42 -0500

    Update ALLOWED_HOSTS to include production and local development domains

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-64$y1@&8hu70-&9=uk=(r!76+^i6qt3rrz$(**bn)wb1=ksg9l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["python-backend-dev-j24l.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
